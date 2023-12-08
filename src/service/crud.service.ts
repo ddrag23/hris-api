5f08898f7a50a8296d9f137482589bd1a509235c
ddrag23 <kurodyami@gmail.com>
2023-12-08 09:19:14 +0700

    :hammer: add: relation

--- src/service/crud.service.ts
+++ src/service/crud.service.ts
@@ -3,6 +3,8 @@ import { Injectable, Inject } from '@nestjs/common';
 
 @Injectable()
 export class CrudService<T, K> {
+  relation: Object | undefined;
+  detailRelation: Object | undefined;
   constructor(
     protected prisma: PrismaService,
     @Inject('model') protected model: string,
@@ -12,11 +14,16 @@ export class CrudService<T, K> {
   }
 
   findAll() {
-    return this.prisma[this.model].findMany();
+    return this.prisma[this.model].findMany(
+      this.relation ? { include: { ...this.relation } } : {},
+    );
   }
 
   findOne(id: number) {
-    return this.prisma[this.model].findUnique({ where: { id } });
+    const include = this.detailRelation
+      ? { include: { ...this.relation } }
+      : {};
+    return this.prisma[this.model].findUnique({ where: { id }, ...include });
   }
 
   update(id: number, updatePositionDto: K) {
