071ba432b49b50b6e243dfbd725f9a8190796064
ddrag23 <kurodyami@gmail.com>
2023-12-08 09:19:25 +0700

    :hammer: add: pagination

--- src/service/prisma.service.ts
+++ src/service/prisma.service.ts
@@ -1,9 +1,33 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PaginationQuery } from 'src/entities/pagination';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
+  }
+  async paginationTransaction(
+    model: string,
+    pq: PaginationQuery,
+  ): Promise<Object> {
+    let start = +pq.skip > 1 ? +pq.skip * +pq.take - +pq.take : 0;
+    const totalData = await this[model].count();
+    let pages = Math.ceil(totalData / +pq.take);
+    const data = await this[model].findMany({
+      take: +pq.take,
+      skip: start,
+      orderBy: {
+        [pq.sortBy]: pq.sortType,
+      },
+      include: { category: true },
+    });
+    const currentPage = start / +pq.take + 1;
+    return {
+      data,
+      pages,
+      currentPage,
+      totalData,
+    };
   }
 }
